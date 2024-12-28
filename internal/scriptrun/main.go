da0baab3511e66a3eed6e34b53403a5f9952cb6f
nua <enuesaa@gmail.com>
2024-12-28 23:16:54 +0900

    feat: create package `scriptrun`

---
+++ internal/scriptrun/main.go
@@ -0,0 +1,29 @@
+package scriptrun
+
+import (
+	"github.com/yuin/gopher-lua"
+)
+
+var ExampleScript = `
+	if string.find(event.path, "7") then
+		print("bbb")
+	end
+	event.path = event.path .. "aaa"
+`
+
+func Run(script string) string {
+	L := lua.NewState()
+	defer L.Close()
+
+	event := L.NewTable()
+	L.SetField(event, "path", lua.LString("example/"))
+	L.SetGlobal("event", event)
+
+	if err := L.DoString(script); err != nil {
+		panic(err)
+	}
+	updatedObj := L.GetGlobal("event").(*lua.LTable)
+	path := L.GetField(updatedObj, "path").(lua.LString)
+
+	return string(path)
+}
