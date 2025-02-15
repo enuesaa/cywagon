45b9ef4d04d56948d4045a571dc218f58ad3ae26
nua <enuesaa@gmail.com>
2025-02-15 20:06:33 +0900

    chore(example): do not print because of noise.

--- testdata/examplecom/default.lua
+++ testdata/examplecom/default.lua
@@ -3,11 +3,9 @@ host = "example.com"
 entry.host = "https://example.com"
 
 function handler(next, req)
-    print('handle')
     if (req.path == "/favicon.ico") then
         req.path = "/aaa"
     end
-    print(req.path)
 
     res = next(req)
     res.status = 201
