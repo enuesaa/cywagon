77c9fa22191de8b8c308db421261e5a5ed0a8a39
nua <enuesaa@gmail.com>
2025-03-29 23:30:17 +0900

    feat: socket でやりとりすると決めた

--- cli/handle/deploy.go
+++ cli/handle/deploy.go
@@ -2,11 +2,22 @@ package handle
 
 import (
 	"fmt"
+	"net"
 	"time"
 )
 
 func (h *Handler) Deploy(sitename string, path string) error {
 	h.Log.Info("start a new deployment..")
+
+	conn, err := net.Dial("unix", sock)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+	conn.Write([]byte("Hello from client"))
+
+	time.Sleep(100 * time.Second)
+
 
 	//  client としてデーモンに指示する形式かな
 
--- cli/handle/up.go
+++ cli/handle/up.go
@@ -1,5 +1,15 @@
 package handle
 
+import (
+	"fmt"
+	"io"
+	"net"
+	"os"
+)
+
+var sock = "/var/run/cywagon.sock"
+
+
 func (h *Handler) Up(paths []string) error {
 	confs, err := h.ConfSrv.List(paths)
 	if err != nil {
@@ -7,5 +17,34 @@ func (h *Handler) Up(paths []string) error {
 	}
 	h.Engine.PrintBanner(confs)
 
+	if err := h.upCreateSocket(); err != nil {
+		return err
+	}
+
 	return h.Engine.Serve(confs)
+}
+
+func (h *Handler) upCreateSocket() error {
+	if _, err := os.Stat(sock); err == nil {
+		return fmt.Errorf("sock exists")
+	}
+
+	listener, err := net.Listen("unix", sock)
+	if err != nil {
+		return err
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			return err
+		}
+		defer conn.Close()
+		buf, err := io.ReadAll(conn)
+		if err != nil {
+			return err
+		}
+		h.Log.Info("Received: %s", string(buf))
+	}
 }
