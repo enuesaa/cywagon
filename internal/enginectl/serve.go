b84d3503b8aafc472144a606f385b1f453162ea8
nua <enuesaa@gmail.com>
2025-04-12 20:13:23 +0900

    fix(libserve): pass site def

--- internal/enginectl/serve.go
+++ internal/enginectl/serve.go
@@ -1,8 +1,23 @@
 package enginectl
 
-import "github.com/enuesaa/cywagon/internal/service/model"
+import (
+	"github.com/enuesaa/cywagon/internal/libserve"
+	"github.com/enuesaa/cywagon/internal/service/model"
+)
 
 func (e *Engine) Serve(config model.Config) error {
+	for _, site := range config.Sites {
+		site := libserve.Site{
+			Host:      site.Host,
+			// Handler: func(res *libserve.HandlerResponse, next libserve.Next, req libserve.HandlerRequest) error {
+			// 	// ここで in memory からデータを読み取る
+				
+			// 	return conf.Handler(res, next, req)
+			// },
+		}
+		e.Server.Sites.Push(site)
+	}
+
 	e.Server.Port = int(config.Server.Port)
 
 	return e.Server.Serve()
