7a7de7a118f261088a974b8db0875102e458b3d5
nua <enuesaa@gmail.com>
2025-04-27 18:33:31 +0900

    refactor(conf): mv format logic to service layer

--- cli/handle/format.go
+++ cli/handle/format.go
@@ -1,30 +1,10 @@
 package handle
 
-import (
-	"strings"
-
-	"github.com/hashicorp/hcl/v2/hclwrite"
-)
-
 func (h *Handler) Format(workdir string) error {
-	fpaths, err := h.Fs.ListFiles(workdir)
-	if err != nil {
+	if err := h.ConfSrv.Format(workdir); err != nil {
 		return err
 	}
-	for _, fpath := range fpaths {
-		if !strings.HasSuffix(fpath, ".hcl") {
-			continue
-		}
-		fbytes, err := h.Fs.Read(fpath)
-		if err != nil {
-			return err
-		}
-		formatted := hclwrite.Format(fbytes)
+	h.Log.Info("formatted!")
 
-		if err := h.Fs.Create(fpath, formatted); err != nil {
-			return err
-		}
-		h.Log.Info(fpath)
-	}
 	return nil
 }
--- internal/service/conf.go
+++ internal/service/conf.go
@@ -7,6 +7,7 @@ import (
 	"github.com/enuesaa/cywagon/internal/libhcl"
 	"github.com/enuesaa/cywagon/internal/service/model"
 	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclwrite"
 )
 
 func NewConfSrv() ConfSrvInterface {
@@ -17,7 +18,9 @@ func NewConfSrv() ConfSrvInterface {
 }
 
 type ConfSrvInterface interface {
+	ListHCLFiles(workdir string) ([]string, error)
 	Read(workdir string) (model.Config, error)
+	Format(workdir string) error
 }
 
 type ConfSrv struct {
@@ -26,16 +29,30 @@ type ConfSrv struct {
 	Hcl libhcl.Parser
 }
 
-func (c *ConfSrv) readHCLFiles(workdir string) (hcl.Body, error) {
-	files := make(map[string][]byte, 0)
+func (c *ConfSrv) ListHCLFiles(workdir string) ([]string, error) {
 	fpaths, err := c.Fs.ListFiles(workdir)
 	if err != nil {
 		return nil, err
 	}
+	var list []string
+
 	for _, fpath := range fpaths {
 		if !strings.HasSuffix(fpath, ".hcl") {
 			continue
 		}
+		list = append(list, fpath)
+	}
+	return list, nil
+}
+
+func (c *ConfSrv) readHCLFiles(workdir string) (hcl.Body, error) {
+	list, err := c.ListHCLFiles(workdir)
+	if err != nil {
+		return nil, err
+	}
+	files := make(map[string][]byte, 0)
+
+	for _, fpath := range list {
 		fbytes, err := c.Fs.Read(fpath)
 		if err != nil {
 			return nil, err
@@ -69,4 +86,23 @@ func (c *ConfSrv) Read(workdir string) (model.Config, error) {
 		return config, err
 	}
 	return config, nil
+}
+
+func (c *ConfSrv) Format(workdir string) error {
+	list, err := c.ListHCLFiles(workdir)
+	if err != nil {
+		return err
+	}
+	for _, fpath := range list {
+		fbytes, err := c.Fs.Read(fpath)
+		if err != nil {
+			return err
+		}
+		formatted := hclwrite.Format(fbytes)
+
+		if err := c.Fs.Create(fpath, formatted); err != nil {
+			return err
+		}
+	}
+	return nil
 }
